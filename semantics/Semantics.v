562473661827c6a9a0ad64a99553659df423e07f
PoloGe <119010079@link.cuhk.edu.cn>
2023-04-08 18:25:05 +0800

    feat: add interp

--- semantics/Semantics.v
+++ semantics/Semantics.v
@@ -1,4 +1,28 @@
 Require Import Frap.Frap.
 
+Inductive expr :=
+| Const (n : nat)
+| Var (x : var)
+| Plus (e1 e2 : expr)
+| Times (e1 e2 : expr).
+
+Example ex1 := Var "y".
+
 Definition valuation := fmap var nat.
 
+Fixpoint interp (e : expr) (v : valuation) : nat :=
+  match e with
+  | Const n => n
+  | Var x =>
+    match v $? x with
+    | None => 0
+    | Some n => n
+    end
+  | Plus e1 e2 => interp e1 v + interp e2 v
+  | Times e1 e2 => interp e1 v * interp e2 v
+  end.
+
+Example val1: valuation := $0 $+ ("x", 17) $+ ("y", 3).
+Example val2: valuation := $0 $+ ("x", 17) $- "x".
+
+Compute interp ex1 val2.
